36333f17a092d3eff6e4b0a44001ce0987883de7
grantguo <810153274@qq.com>
2022-09-26 22:20:29 +0800

    cli

--- page/index/index.vue
+++ page/index/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="main">
-        {{ msg }}   
+        {{ msg }}
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            msg: 'hello uniapp vue2'
+            msg: 'hello uniapp vue2'   
         }
     }
 }
