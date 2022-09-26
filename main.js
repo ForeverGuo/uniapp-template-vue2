933ee286e5ee7bc2cd81e24ce9f37566c1206c7b
grantguo <810153274@qq.com>
2022-09-26 22:52:10 +0800

    cli

--- main.js
+++ main.js
@@ -9,18 +9,16 @@ import wxIsLogin from '@/components/wx_login/wx_login.js'
 import uView from 'uview-ui'
 Vue.use(uView)
 
-var ss = "134"
-
 Vue.config.productionTip = false
 
 Vue.mixin({
-    mixins: [share, wxIsLogin]
+  mixins: [share, wxIsLogin]
 })
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-    store
+  ...App,
+  store
 })
 app.$mount()
--- page/index/index.vue
+++ page/index/index.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="main">
-        {{ msg }}
-    </div>
+  <div class="main">
+    {{ msg }}
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            msg: 'hello uniapp vue2'   
-        }
+  data() {
+    return {
+      msg: 'hello uniapp vue2'
     }
+  }
 }
 </script>
 <style scoped lang="scss">
 .main {
-    padding: 300rpx 0;
-    text-align: center;
+  padding: 300rpx 0;
+  text-align: center;
 }
 </style>
