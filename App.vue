af6e47d5db9d3236465f301c6c2e14501f9e706b
guoys <guoys@91160.com>
2022-09-29 14:03:05 +0800

    fix --

--- App.vue
+++ App.vue
@@ -1,49 +1,46 @@
-
 <script>
-	import store from "@/store/store.js"
 	export default {
-		onLaunch: function() {
+		onLaunch: function () {
 			console.log('App Launch')
-			const updateManager = uni.getUpdateManager();
-			
+			const updateManager = uni.getUpdateManager()
+	
 			updateManager.onCheckForUpdate(function (res) {
-			  // 请求完新版本信息的回调
-			  console.log(res.hasUpdate);
-			});
-			
+				// 请求完新版本信息的回调
+				console.log(res.hasUpdate)
+			})
+	
 			updateManager.onUpdateReady(function (res) {
-			  uni.showModal({
-			    title: '更新提示',
-			    content: '新版本已经准备好，是否重启应用？',
-			    success(res) {
-			      if (res.confirm) {
-			        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-			        updateManager.applyUpdate();
-			      }
-			    }
-			  });
-			
-			});
-			
+				uni.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate()
+						}
+					}
+				})
+			})
+	
 			updateManager.onUpdateFailed(function (res) {
-			  // 新的版本下载失败
-			});
-			
+				// 新的版本下载失败
+			})
 		},
-		onShow: async function(data) {
+		onShow: async function (data) {
 			wx.hideHomeButton()
 			uni.hideTabBar()
 		},
-		onHide: function() {
+		onHide: function () {
 			console.log('App Hide')
-		},
+		}
 	}
-</script>
-
-<style>
+	</script>
+	
+	<style>
 	/*每个页面公共css */
-</style>
-<style lang="scss">
-	@import "uview-ui/index.scss";
+	</style>
+	<style lang="scss">
+	@import 'uview-ui/index.scss';
 	@import '@/common/css/common.scss';
-</style>
+	</style>
+	
